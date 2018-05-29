2765f69e0181280970f72bf6db6185b265a9b553
Bianca Hale <sashabtt@gmail.com>
2018-05-29 16:24:53 -0600

    crud actions

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,48 @@
+class ClientsController < ApplicationController
+  def index
+    @clients = Client.all
+  end
+
+  def show
+    @client = Client.find(params[:id])
+  end
+
+  def new
+    @client = Client.new
+  end
+
+  def create
+    @client = Client.new(client_params)
+
+    if @client.save
+      redirect_to clients_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @client = Client.find(params[:id])
+  end
+
+  def update
+    @client = Client.find(params[:id])
+    if @client.updates(client_params)
+      redirect_to clients_path
+    else 
+      render :edit
+    end
+  end
+
+  def destroy
+    Client.find(params[:id]).destroy
+    redirect_to clients_path
+  end
+
+  private
+
+  def client_params
+    params.require(:client).permit(:name, :age, :hair_color, :eye_color, :gender, :alive)
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+hair_colors = ['red','blonde', 'brown', 'black', 'grey']
+eye_colors = ['brown', 'hazel', 'green', 'blue', 'black']
+genders = ['male', 'female']
+
+10.times do
+  Client.create(
+    name: Faker::Name.name_with_middle,
+    age: Faker::Number.between(1, 10),
+    alive: Faker::Boolean.boolean,
+    hair_color: hair_colors.sample,
+    eye_color: eye_colors.sample,
+    gender: genders.sample,
+  )
+end
+
+puts "Clients seeded"
